6e6fc95d16c038306f79105c2a6071e94956bb7c
Selina Boulic <selina.boulic@gmail.com>
2019-02-04 10:21:28 +0100

    removed useless parts + changed validate function

--- cozy-konnector-OB/src/index.js
+++ cozy-konnector-OB/src/index.js
@@ -1,131 +1,80 @@
+
+// Declaration of constants from 'cozy-konnector-libs' library. Others?
 const {
   BaseKonnector,
   requestFactory,
   signin,
   scrape,
-  saveBills,
   log
 } = require('cozy-konnector-libs')
+
 const request = requestFactory({
-  // the debug mode shows all the details about http request and responses. Very useful for
-  // debugging but very verbose. That is why it is commented out by default
-  debug: true,
+  // debug mode to show all details about http requests and responses
+  // Very verbose though. Uncomment if needed.
+  // debug: true,
   // activates [cheerio](https://cheerio.js.org/) parsing on each page
   cheerio: true,
-  // If cheerio is activated do not forget to deactivate json parsing (which is activated by
-  // default in cozy-konnector-libs
+  // json parsing (activated by default in cozy-konnector-libs but
+  // must be deactivated when using cheerio)
   json: false,
   // this allows request-promise to keep cookies between requests
   jar: true
 })
 
-// TOCHECK : url of the website to connect to ? Change fields in
-// konnector-dev-config.json
 const baseUrl = 'https://openbadges.org'
 
 module.exports = new BaseKonnector(start)
 
-// The start function is run by the BaseKonnector instance only when it got all the account
-// information (fields). When you run this connector yourself in "standalone" mode or "dev" mode,
-// the account information come from ./konnector-dev-config.json file
-async function start(fields) {
-  log('info', 'Authenticating ...')
-  await authenticate(fields.login, fields.password)
-  log('info', 'Successfully logged in')
-  // The BaseKonnector instance expects a Promise as return of the function
-  /*log('info', 'Fetching the list of documents')
-  const $ = await request(`${baseUrl}/index.html`)*/
-  // cheerio (https://cheerio.js.org/) uses the same api as jQuery (http://jquery.com/)
-  /*log('info', 'Parsing list of documents')
-  const documents = await parseDocuments($)*/
+/* DEFINITION OF FUNCTIONS */
 
-  // here we use the saveBills function even if what we fetch are not bills, but this is the most
-  // common case in connectors
-  /*log('info', 'Saving data to Cozy')
-  await saveBills(documents, fields, {
-    // this is a bank identifier which will be used to link bills to bank operations. These
-    // identifiers should be at least a word found in the title of a bank operation related to this
-    // bill. It is not case sensitive.
-    identifiers: ['books']
-  })*/
+// start function run by BaseKonnector instance only when all account
+// information has been provided (fields in 
+// ../konnector-dev-config.json file)
+
+async function start(fields) {
+  log('info', 'Authenticating...')
+  await authenticate (fields.login, fields.password)
+  log('info', 'Successfully logged in')
+  // add actions when successfully logged in
+
+  log('info','fetching web page content')
+  const $ = await request(`${baseUrl}`)
+  log('info', 'parsing content', $)
+
 }
 
-// this shows authentication using the [signin function](https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#module_signin)
-// even if this in another domain here, but it works as an example
-function authenticate(username, password) {
+
+function authenticate(email, password) {
   return signin({
     url: `https://backpack.openbadges.org/backpack/login`,
     formSelector: ('section#login', 'form'),
-    formData: { email: username, password: password },
-    // the validate function will check if the login request was a success. Every website has
-    // different ways respond: http status code, error message in html ($), http redirection
-    // (fullResponse.request.uri.href)...
+    formData: { email: email, password: password },
+    // validate function checks if login was a success
     validate: (statusCode, $, fullResponse) => {
-      log(
-        'debug',
-        fullResponse.request.uri.href,
-        'not used here but should be usefull for other connectors'
-      )
-      // The login in toscrape.com always works excepted when no password is set
-      if ($(`a[href='/logout']`).length === 1) {
+      log('debug', 'status CODE', statusCode)
+      log('debug', 'error mESSAGE', $)
+      log('debug', 'full response', fullResponse)
+      /* 
+       * returned statusCode == 200 for both success and failure
+       * need to consider which web file (see browser console ->
+       * network) is returned : 
+       * https://backpack.openbadges.org in case of success => true
+       * https://backpack.openbadges.org/backpack/login  for failure
+       * => false
+       *
+       * TODO how to print correctly content of $ and fullResponse ?
+       * (if exist ?)
+       *
+       */
+      // sometimes, we can get a http error message ($) ?
+      if (statusCode == 200) {
+        // TO MODIFY
         return true
-      } else {
-        // cozy-konnector-libs has its own logging function which format these logs with colors in
-        // standalone and dev mode and as JSON in production mode
-        log('error', $('.error').text())
-        return false
-      }
-    }
+			}
+  	}
   })
 }
 
-// The goal of this function is to parse a html page wrapped by a cheerio instance
-// and return an array of js objects which will be saved to the cozy by saveBills (https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#savebills)
-function parseDocuments($) {
-  // you can find documentation about the scrape function here :
-  // https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#scrape
-  const docs = scrape(
-    $,
-    {
-      title: {
-        sel: 'h3 a',
-        attr: 'title'
-      },
-      amount: {
-        sel: '.price_color',
-        parse: normalizePrice
-      },
-      fileurl: {
-        sel: 'img',
-        attr: 'src',
-        parse: src => `${baseUrl}/${src}`
-      },
-      filename: {
-        sel: 'h3 a',
-        attr: 'title',
-        parse: title => `${title}.jpg`
-      }
-    },
-    'article'
-  )
-  return docs.map(doc => ({
-    ...doc,
-    // the saveBills function needs a date field
-    // even if it is a little artificial here (these are not real bills)
-    date: new Date(),
-    currency: '€',
-    vendor: 'template',
-    metadata: {
-      // it can be interesting that we add the date of import. This is not mandatory but may be
-      // useful for debugging or data migration
-      importDate: new Date(),
-      // document version, useful for migration after change of document structure
-      version: 1
-    }
-  }))
-}
 
-// convert a price string to a float
-function normalizePrice(price) {
-  return parseFloat(price.replace('£', '').trim())
-}
+
+
