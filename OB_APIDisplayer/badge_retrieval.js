0d7234014718f72ddab468ac72bb8bdfe9591c08
Selina Boulic <selina.boulic@gmail.com>
2019-03-10 12:07:38 +0100

    re wrote part to write badges in files (Promise.all)

--- OB_APIDisplayer/badge_retrieval.js
+++ OB_APIDisplayer/badge_retrieval.js
@@ -5,99 +5,51 @@
 var rp = require('request-promise');
 var fs = require('fs');
 
-var baseUri = 'https://backpack.openbadges.org/displayer/';
 
-function getGroupsBadges (earnerId, options2, folderToWrite) {
-  options2.uri = baseUri+earnerId+'/groups';
-  return rp(options2)
-    .then((res) => {
-      var recData = JSON.parse(res);
-      console.debug('2nd res ', recData);
-      var options_list = [];
-      var groupsList = recData.groups;
-      for (var i=0; i<groupsList.length; i++) {
-        options_list.push({uri: baseUri+earnerId+'/group/'+groupsList[i].groupId, method: 'GET'});
-      }
-      //console.debug('here option_list ', options_list);
-      // getBadges allows to do a request for each badge group, thanks to recursivity
-      return writeBadgesInFiles (options_list, groupsList, folderToWrite)
-    })
+async function getUserId(conv_options) {
+/* 
+ * POST request to OB's email-to-id conversion service :
+ * from user's email address (specified in conv_options), function returns user's id
+ */
+  const res = await rp(conv_options);
+  return JSON.parse(res).userId
 }
 
-function getUserId(options1) {
-  return rp(options1)
-    .then((res) => {
-      var recData = JSON.parse(res);
-      return recData.userId
-    })
-}
+
+async function getGroupsBadges (earnerId, earnerFolder) {
+/* 
+ * Step 1: getting groups list
+ * The GET request returns a JSON string. Attribute 'groups' from the corresponding object contains the list of the user's badges groups, each being defined by an object with 3 attributes (groupId, name, badges), attribute "badges" giving the number of badges in the group. See doc.
+ *
+ * Step 2 : Writing the content of each badges group in a file : earnerId+'/'+group.name
+ */
+  var getList_options = {uri: 'https://backpack.openbadges.org/displayer/'+earnerId+'/groups', method: 'GET'};
+  const res1 = await rp(getList_options)
+  const groupsList = JSON.parse(res1).groups;
+
+  await Promise.all(groupsList.map(async (group) => {
+    // this structure is supposed to allow multiple asynchronous calls in parallel
+    console.log('awaiting some fun stuff');
+    const res2 = await rp({uri: 'https://backpack.openbadges.org/displayer/'+earnerId+'/group/'+group.groupId, method: 'GET'})
+    fs.writeFileSync(earnerFolder+'/'+group.name, res2, () => {
+    console.log('file written !');    
+    });
+  }))
+  console.log('all data has been retrieved');
+
 /*
-async function writeBadgesInFiles (options_list, groupsList, folder) {
-  // si options_list est (encore) non vide on refait la request avec les options du premier objet de la liste et on retire ce dernier de options_list à la fin de l'opération
-  if (options_list[0]) {
-    var res = await rp(options_list[0]);
-    fs.writeFileSync(folder+'/'+groupsList[0].name, res, () => {console.log('file written !')});
-    options_list.shift(); //remove first element
-    groupsList.shift();
-    writeBadgesInFiles (options_list, groupsList, folder);
-  } else {
-    return console.log('c est fini !!!!!!!!!');
+  for (const group of groupsList) {
+  // this structure allows multiple asynchronous calls in sequence
+    const res2 = await rp({uri: 'https://backpack.openbadges.org/displayer/'+earnerId+'/group/'+group.groupId, method: 'GET'})
+    fs.writeFileSync(earnerFolder+'/'+group.name, res2, () => {
+    console.log('file written !');
+    });
   }
+ */
+
 }
-*/
+
+
 module.exports.getUserId = getUserId;
 module.exports.getGroupsBadges = getGroupsBadges;
-
-// OU
-/*
-async function executeAsyncTask() {
-  const valueA = await functionA()
-  const valueB = await functionB(valueA)
-  return function3(valueA, valueB)
-}
-*/
-
-
-/*
-function getGroupsBadges (options1, options2) {
-  console.log('Inside the function!');
-  var earnerId;
-  return rp(options1)
-    .then(function (res) {
-      var recData = JSON.parse(res);
-      earnerId = recData.userId;
-      //console.debug('returnedRes', earnerId);
-      options2.uri = baseUri+earnerId+'/groups';
-      return rp(options2)
-    })
-    .then(function (res) {
-      var recData = JSON.parse(res);
-      console.debug('2nd res ', recData);
-      var options_list = [];
-      var groupsList = recData.groups;
-      for (var i=0; i<groupsList.length; i++) {
-        options_list.push({uri: baseUri+earnerId+'/group/'+groupsList[i].groupId, method: 'GET'});
-      }
-      //console.debug('here option_list ', options_list);
-      // getBadges allows to do a request for each badge group, thanks to recursivity
-      return writeBadgesInFiles (options_list, groupsList, earnerId)
-    })
-}
-*/
-
-
-function writeBadgesInFiles (options_list, groupsList, folder) {
-  // si options_list est (encore) non vide on refait la request avec les options du premier objet de la liste et on retire ce dernier de options_list à la fin de l'opération
-  if (options_list[0]) {
-    return rp(options_list[0])
-      .then ((res) => {
-        fs.writeFileSync(folder+'/'+groupsList[0].name, res, () => {console.log('file written !')});
-        options_list.shift(); //remove first element
-        groupsList.shift();
-        writeBadgesInFiles (options_list, groupsList, folder);
-      })
-  } else {
-    console.log('c est fini !!!!!!!!!');
-  }
-}
 
